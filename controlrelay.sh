38c9e8c9f313b8da5317328a173e097f8e43f26c
adelorenzo <adolfo@delorenzo.mobi>
2017-04-19 16:55:15 -0300

    Update controlrelay.sh

--- controlrelay.sh
+++ controlrelay.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Checks for presence of device and speaks a notification
+# Checks for presence of device and speaks and controls a relay module attached via GPIO
 
 while true
 do
 
-    #ping -c 1 -W 5 192.168.1.150 > $HOME/.devicelist
+    #ping -c 1 -W 5 192.168.1.150 > $HOME/.devicelist -> this is experimental and requires that you set a fixed ip address for your mobile phone
     l2ping -c 1 -t 20 60:E3:AC:14:DB:EB > $HOME/.devicelist
     status=$(grep -i -c '1 received' $HOME/.devicelist)
     prevStatus=$(cat $HOME/.ishome)
@@ -15,11 +15,9 @@ do
         if [ $status = "1" ]; then
             echo "Phone detected"
 	    gpio write 1 1
-	    gpio write 2 1
         else
             echo "Phone lost"
 	    gpio write 1 0
-	    gpio write 2 0
         fi
     fi
 
